d06db1b0489442a8ea1708cf4e70fa7b56a3b2c7
Gyper1on <Dzmiter@list.ru>
2023-10-03 00:29:09 +0300

    hoock "useRef"

---
+++ learning-react-hooks/src/useRef/programmablActionsExample.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+const ProgrammablActionsExample = () => {
+    return (
+        <>
+        <h1>Программируемые действия и свойства</h1>
+
+
+        </>
+    )
+}
+
+
+export default ProgrammablActionsExample
