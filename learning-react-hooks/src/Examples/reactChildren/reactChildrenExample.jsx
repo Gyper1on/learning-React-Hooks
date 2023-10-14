ec537b95f2761a0199ac174d53934c95b4f5fe26
Gyper1on <Dzmiter@list.ru>
2023-10-14 22:52:57 +0300

    learning props.children

--- learning-react-hooks/src/Examples/reactChildren/reactChildrenExample.jsx
+++ learning-react-hooks/src/Examples/reactChildren/reactChildrenExample.jsx
@@ -1,20 +1,29 @@
-import React, {useState} from "react";
-import prevState from "../useRef/prevState";
+import React, {useEffect, useState} from "react";
 import TextField from "../../common/form/textField";
 import PropTypes from "prop-types";
 
 
 const FormComponent = ({children}) => {
     const [data, setData] = useState({})
-    // const handleChange = ( target) => {
-    //     setData( prevState => ({...prevState, [target.name] : target.value}))
-    // }
+    useEffect(() => {
+        console.log(data)}, [data])
+    const handleChange = ( target) => {
+        setData( prevState => ({...prevState, [target.name] : target.value}))
+    }
 
-    return children
+
+    return  React.Children.map( children,(child) => {
+        const config = {...child.props,
+            onChange: handleChange,
+            value: data[child.props.name] || ''
+        }
+
+        return   React.cloneElement(child, config)
+    })
 }
 
 FormComponent.propTypes = {
-    children: PropTypes.oneOfType([PropTypes.oneOf(PropTypes.node),
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node),
     PropTypes.node])
 }
 
@@ -22,9 +31,10 @@ const ReactChildrenExample = () => {
     return (
         <>
         <h1>Clone form and add props</h1>
+            <FormComponent>
             <TextField name='email' label = 'email'/>
             <TextField name='password' label = 'Пароль' type ='password'/>
-
+            </FormComponent>
         </>
     )
 }
